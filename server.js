5354462f3f7b7a0f4195354f22fecd3165da5d50
Yvonne Hayes <queyvonne@gmail.com>
2016-05-16 12:12:48 -0700

    getting started. Trying out a switch statement. Got case for homepage

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const http = require ( 'http' );
+const url = require ( 'url' );
+
+function pageMessages(req, res) {
+
+  //need to check first word of URL
+  var pathname = url.parse(req.url).pathname.split('/')[1]; //cleaning up URL and getting first word
+
+  switch (pathname) {
+    case '':
+      res.writeHead(200, {Content-Type: "text/plain"});
+      res.write('Welcome to the home page');
+      res.end();
+      break;
+
+
+  }
+};
