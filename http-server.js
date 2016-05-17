57c3436b3546ff06867e107bdcd517bf9a1c5dae
Johnny Luangphasy <johnny@jluangphasy.com>
2016-05-17 13:17:09 -0700

    Changed paths to farewells and greetings, created json for list of farewells and greetings

--- http-server.js
+++ http-server.js
@@ -25,27 +25,31 @@ httpServer.formatContent = function (urlParse, content) {
 httpServer.new = function (portNumber = 4000) {
   http.createServer((req, res) => {
     if (req.method === 'GET') {
+      let statusCode = 200;
       let urlParse = url.parse(req.url, true);
       let bodyContent;
       let urlBase = `http://localhost:${portNumber}`;
-      let urlHello = `${urlBase}/hello`;
-      let urlGoodbye = `${urlBase}/goodbye`;
+      let urlGreetings = `${urlBase}/greetings`;
+      let urlFarewells = `${urlBase}/farewells`;
 
       switch (urlParse.pathname) {
-      case '/hello':
-        bodyContent = httpServer.formatContent(urlParse, 'Hello World');
-        break;
-      case '/goodbye':
+      case '/farewells':
         bodyContent = httpServer.formatContent(urlParse, 'Goodbye World');
         break;
+      case '/greetings':
+        bodyContent = httpServer.formatContent(urlParse, 'Hello World');
+        break;
       default:
+        statusCode = 400;
         bodyContent = '<p>In your browswer, visit ' +
-          `<a href="${urlHello}">${urlHello}</a> or ` +
-          `<a href="${urlGoodbye}">${urlGoodbye}</a>.</p>`;
+          `<a href="${urlGreetings}">${urlGreetings}</a> or ` +
+          `<a href="${urlFarewells}">${urlFarewells}</a>.</p>`;
         break;
       }
 
-      res.writeHead(200, { 'content-type': 'text/html' });
+      res.writeHead(statusCode, {
+        'content-type': 'text/html'
+      });
       res.end(bodyContent);
     }
   }).listen(portNumber);
