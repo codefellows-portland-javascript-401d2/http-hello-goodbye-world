cc54037f1df40b84227ff03309523cb85a292271
DonChatelain <djchatelain@gmail.com>
2016-05-17 17:50:30 -0700

    router now picks out pathname even in the case of query strings in url

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
 const http = require('http');
+const url = require('url');
+const queryStr = require('querystring');
 const router = require('./router');
 
 var server = http.createServer((request, response) => {
-
-  router.route(request.url, response);
   
-  // if (request.url === '/hello') {
-  //   response.writeHead(200, {'Content-Type': 'text/html'});
-  //   fs.createReadStream('html/hello.html').pipe(response);
-  // } else if (request.url === '/goodbye') {
-  //   response.writeHead(200, {'Content-Type': 'text/html'});
-  //   fs.createReadStream('html/goodbye.html').pipe(response);
-  // } else if (request.url === '/') {
-  //   response.writeHead(200, {'Content-Type': 'text/html'});
-  //   fs.createReadStream('html/index.html').pipe(response);
-  // } else {
-  //   response.writeHead(404, {'Content-Type': 'text/html'});
-  //   fs.createReadStream('html/404.html').pipe(response);
-  // }
+  const thisURL = url.parse(request.url, true);
+  const thisPath = thisURL.pathname;
+  const thisQuery = thisURL.query;
+  router.route(thisPath, response);
+  // response.end('on it');
+  
 });
-
 
 module.exports = server;
