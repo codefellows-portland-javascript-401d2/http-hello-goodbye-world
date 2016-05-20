381dce5f94aae1c3fb31dc2d06fb25beb8131435
Yvonne Hayes <queyvonne@gmail.com>
2016-05-19 20:12:54 -0700

    added more tests to get more exact response message. Also added 4th test to test the /date page

--- test/server.test.js
+++ test/server.test.js
@@ -8,12 +8,14 @@ chai.use( chaiHttp );
 describe( 'http server', () => {
 
   const request = chai.request (server);
+  var date = new Date();
 
   it( 'sends back index response', done => {
     request
       .get('/')
       .end( (err,res ) => {
         assert.equal( res.statusCode, 200);
+        assert.equal( res.text, 'Welcome to the home page');
         assert.ok( res.text );
         done();
       });
@@ -24,6 +26,18 @@ describe( 'http server', () => {
       .get('/secret')
       .end( (err,res ) => {
         assert.equal( res.statusCode, 200);
+        assert.equal( res.text, 'Oh-oh. You have found the super secret page. This message will self-destruct in 3 - 2 - 1 -  BOOM!');
+        assert.ok( res.text );
+        done();
+      });
+  });
+
+  it( 'sends back date', done => {
+    request
+      .get('/date')
+      .end( (err,res ) => {
+        assert.equal( res.statusCode, 200);
+        assert.equal( res.text, date);
         assert.ok( res.text );
         done();
       });
@@ -34,6 +48,7 @@ describe( 'http server', () => {
       .get('/kittykat')
       .end( (err,res ) => {
         assert.equal( res.statusCode, 400);
+        assert.equal( res.text, 'ERROR 400 - Your request was bad and you should feel bad!');
         assert.ok( res.text );
         done();
       });
